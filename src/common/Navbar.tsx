e2d7810b34d60d7e287c7e5ba031fde4eac6c9a3
Raichung Pun Magar <raichung.magar@gmail.com>
2023-12-21 16:53:16 +0545

    addition of publication in navbar

--- src/common/Navbar.tsx
+++ src/common/Navbar.tsx
@@ -9,10 +9,10 @@ const Navlink: React.FC = () => {
         <Link to="/about">About</Link>
       </li>
       <li className="cursor-pointer text-lg text-primary-blueText  hover:bg-primary-blueText hover:text-white rounded py-2 px-2 font-kameron">
-        Publication
+        <Link to="/publications">Publication</Link>
       </li>
       <li className="cursor-pointer text-lg text-primary-blueText  hover:bg-primary-blueText hover:text-white rounded py-2 px-2 font-kameron">
-      <Link to="/projects">Projects</Link>
+        <Link to="/projects">Projects</Link>
       </li>
       <div className="hidden md:block">
         <Logo />
