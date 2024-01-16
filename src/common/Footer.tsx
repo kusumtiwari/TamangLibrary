42bf3825b1a820331035a8abc6bfa66fa62bb1aa
Raichung Pun Magar <raichung.magar@gmail.com>
2024-01-17 01:30:17 +0545

    deployment fixes

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -6,115 +6,119 @@ import { MdOutlineCopyright } from "react-icons/md";
 import { Link } from "react-router-dom";
 import "./../utils/custom.css";
 const Footer: React.FC = () => {
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
-  return (
-    <div className="flex flex-col mt-28">
-      <div className="relative flex-1 min-h-[75vh] footer">
-        <div className="absolute top-0 left-0 z-0 bg-[url('/img/common/Footer.png')] bg-cover bg-no-repeat w-full h-[50vh] object-cover flex items-center justify-center"></div>
-        <div className="absolute top-36 left-0 z-10 bg-secondary-detailsBackground w-full py-16 px-8 md:px-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 flex-grow">
-          <div className="">
-            <h1 className="uppercase text-2xl text-primary-blueText py-2">
-              Get in Touch
-            </h1>
-            <div className="flex items-center text-black font-cameron md:text-xl py-2">
-              <IoLocationOutline className="w-5 h-5" />
-              <h1 className="ml-2">Boudha - 6, Kathmandu Nepal</h1>
-            </div>
-            <div className="flex items-center text-black font-kameron md:text-xl py-2">
-              <IoCallOutline className="w-5 h-5" />
-              <h1 className="ml-2">9851082503 | 9841474917</h1>
-            </div>
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+	};
+	return (
+		<div className="flex flex-col mt-28">
+			<div className="relative flex-1 min-h-[75vh] footer">
+				<div className="absolute top-0 left-0 z-0 bg-[url('/img/common/Footer.png')] bg-cover bg-no-repeat w-full h-[50vh] object-cover flex items-center justify-center"></div>
+				<div className="absolute top-36 left-0 z-10 bg-secondary-detailsBackground w-full py-16 px-8 md:px-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 flex-grow">
+					<div className="">
+						<h1 className="uppercase text-2xl text-primary-blueText py-2">
+							Get in Touch
+						</h1>
+						<div className="flex items-center text-black font-cameron md:text-xl py-2">
+							<IoLocationOutline className="w-5 h-5" />
+							<h1 className="ml-2">Boudha - 6, Kathmandu Nepal</h1>
+						</div>
+						<div className="flex items-center text-black font-kameron md:text-xl py-2">
+							<IoCallOutline className="w-5 h-5" />
+							<h1 className="ml-2">9851082503 | 9841474917</h1>
+						</div>
 
-            <div className="flex items-center text-black font-kameron md:text-xl py-2">
-              <CiMail className="w-5 h-5" />
-              <h1 className="ml-2">contact@tamanglibrary.org</h1>
-            </div>
-          </div>
+						<div className="flex items-center text-black font-kameron md:text-xl py-2">
+							<CiMail className="w-5 h-5" />
+							<h1 className="ml-2">contact@tamanglibrary.org</h1>
+						</div>
+					</div>
 
-          <div className="lg:mx-auto cursor-pointer">
-            <p className="md:text-xl py-2">
-              <Link to="/" onClick={scrollToTop}>
-                Home
-              </Link>
-            </p>
-            <p className="md:text-xl py-2">
-              <Link to="/projects" onClick={scrollToTop}>
-                Projects
-              </Link>
-            </p>
-            <p className="md:text-xl py-2">
-              <Link to="/publications" onClick={scrollToTop}>
-                Publication
-              </Link>
-            </p>
-            <p className="md:text-xl py-2">
-              <Link to="/collection" onClick={scrollToTop}>
-                Collection
-              </Link>
-            </p>
-          </div>
+					<div className="lg:mx-auto cursor-pointer">
+						<p className="md:text-xl py-2">
+							<Link to="/" onClick={scrollToTop}>
+								Home
+							</Link>
+						</p>
+						<p className="md:text-xl py-2">
+							<Link to="/projects" onClick={scrollToTop}>
+								Projects
+							</Link>
+						</p>
+						<p className="md:text-xl py-2">
+							<Link to="/publications" onClick={scrollToTop}>
+								Publication
+							</Link>
+						</p>
+						<p className="md:text-xl py-2">
+							<Link to="/collection" onClick={scrollToTop}>
+								Collection
+							</Link>
+						</p>
+					</div>
 
-          <div className="lg:mx-auto cursor-pointer">
-            <p className="md:text-xl py-2">
-              <Link to="/about" onClick={scrollToTop}>
-                About
-              </Link>
-            </p>
-            <p className="md:text-xl py-2">Gallery</p>
-            <p className="md:text-xl py-2">
-              <Link to="/events" onClick={scrollToTop}>
-                Events
-              </Link>
-            </p>
-            <p className="md:text-xl py-2">
-              <Link to="/contact" onClick={scrollToTop}>
-                Contact
-              </Link>
-            </p>
-          </div>
+					<div className="lg:mx-auto cursor-pointer">
+						<p className="md:text-xl py-2">
+							<Link to="/about" onClick={scrollToTop}>
+								About
+							</Link>
+						</p>
+						<p className="md:text-xl py-2">
+							<Link to="/events" onClick={scrollToTop}>
+								Events
+							</Link>
+						</p>
+						<p className="md:text-xl py-2">
+							<Link to="/contact" onClick={scrollToTop}>
+								Contact
+							</Link>
+						</p>
+						<p className="md:text-xl py-2">
+							<Link to="/admin/signin" onClick={scrollToTop}>
+								Admin
+							</Link>
+						</p>
+					</div>
 
-          <div className="">
-            <h1 className="uppercase text-2xl text-primary-blueText py-2">
-              For update
-            </h1>
-            <input
-              type="text"
-              name="email"
-              placeholder="E-mail"
-              className="border-2 border-primary-blueText px-6 py-2 rounded my-2 w-[80%]"
-            />
-            <div className="">
-              <button className="rounded px-6 py-2 bg-red-800 text-white text-sm">
-                Subscribe
-              </button>
-            </div>
-            <div className="flex items-center text-primary-blueText mt-6">
-              <RiFacebookCircleLine className="w-10 h-10 lg:mr-4" />
-              <FaInstagram className="w-10 h-10" />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="w-full flex flex-col z-20 md:flex-row sm:items-center sm:pb-8 justify-between py-8 px-4 md:px-12">
-        <div className="flex items-center pb-6 md:pb-0">
-          <MdOutlineCopyright className="w-5 h-5 text-primary-blueText mr-2" />
-          <p className="text-base text-primary-blueText">
-            2023 Tamang Library. All right reserved
-          </p>
-        </div>
-        <div className="flex items-center ">
-          <p className="text-base text-primary-blueText mr-2">
-            Designed and developed by
-          </p>
-          <img src="/img/one-window-logo.png" alt="company-logo" />
-        </div>
-      </div>
-    </div>
-  );
+					<div className="">
+						<h1 className="uppercase text-2xl text-primary-blueText py-2">
+							For update
+						</h1>
+						<input
+							type="text"
+							name="email"
+							placeholder="E-mail"
+							className="border-2 border-primary-blueText px-6 py-2 rounded my-2 w-[80%]"
+						/>
+						<div className="">
+							<button className="rounded px-6 py-2 bg-red-800 text-white text-sm">
+								Subscribe
+							</button>
+						</div>
+						<div className="flex items-center text-primary-blueText mt-6">
+							<RiFacebookCircleLine className="w-10 h-10 lg:mr-4" />
+							<FaInstagram className="w-10 h-10" />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="w-full flex flex-col z-20 md:flex-row sm:items-center sm:pb-8 justify-between py-8 px-4 md:px-12">
+				<div className="flex items-center pb-6 md:pb-0">
+					<MdOutlineCopyright className="w-5 h-5 text-primary-blueText mr-2" />
+					<p className="text-base text-primary-blueText">
+						2023 Tamang Library. All right reserved
+					</p>
+				</div>
+				<div className="flex items-center ">
+					<p className="text-base text-primary-blueText mr-2">
+						Designed and developed by
+					</p>
+					<img src="/img/one-window-logo.png" alt="company-logo" />
+				</div>
+			</div>
+		</div>
+	);
 };
 export default Footer;
--- src/pages/admin/projects/index.tsx
+++ src/pages/admin/projects/index.tsx
@@ -30,9 +30,15 @@ import { IoIosCreate } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import { db } from "../../../../firebase";
+import { useEffect, useState } from "react";
 
 const AdminProject = () => {
 	const [data, loading, error] = useCollectionData(collection(db, "projects"));
+	const [initialRenderComplete, setInitialRenderComplete] = useState(false);
+
+	useEffect(() => {
+		setInitialRenderComplete(true);
+	}, []);
 
 	if (loading) {
 		return <LoadingSpinner />;
@@ -50,80 +56,86 @@ const AdminProject = () => {
 
 	return (
 		<>
-			<div>
-				<h1 className="text-3xl font-bold mb-8 pl-2">Projects</h1>
-				<div className="flex justify-end p-4">
-					<Link to="/admin/projects/create">
-						<div className="flex gap-2 items-center border border-black rounded-md p-2">
-							Create New <IoIosCreate />
-						</div>
-					</Link>
-				</div>
+			{initialRenderComplete && (
+				<div>
+					<h1 className="text-3xl font-bold mb-8 pl-2">Projects</h1>
+					<div className="flex justify-end p-4">
+						<Link to="/admin/projects/create">
+							<div className="flex gap-2 items-center border border-black rounded-md p-2">
+								Create New <IoIosCreate />
+							</div>
+						</Link>
+					</div>
 
-				<Table>
-					{/* <TableCaption>A list of your recent invoices.</TableCaption> */}
-					<TableHeader>
-						<TableRow>
-							<TableHead className="w-60">Title</TableHead>
-							<TableHead className="max-w-1/3">Description</TableHead>
-							<TableHead className="max-w-24">Status</TableHead>
-							<TableHead className="text-right">Actions</TableHead>
-						</TableRow>
-					</TableHeader>
-					<TableBody>
-						{data?.map((project) => (
-							<TableRow className="border-2" key={nanoid()}>
-								<TableCell className="border-2 p-2">{project.title}</TableCell>
-								<TableCell
-									className="border-2"
-									dangerouslySetInnerHTML={{
-										__html: truncateOverview(project.description, 185),
-									}}
-								/>
-								<TableCell className="border-2 p-2">{project.status}</TableCell>
-								<TableCell className="flex flex-row gap-2 justify-center items-center align-middle">
-									<Link to={`/admin/projects/edit/${project.id}`}>
-										<Button variant="outline">
-											<AiOutlineEdit />
-										</Button>
-									</Link>
-
-									<AlertDialog>
-										<AlertDialogTrigger asChild>
-											<Button variant="destructive">
-												<AiOutlineDelete />
-											</Button>
-										</AlertDialogTrigger>
-										<AlertDialogContent>
-											<AlertDialogHeader>
-												<AlertDialogTitle>
-													Are you absolutely sure?
-												</AlertDialogTitle>
-												<AlertDialogDescription>
-													This action cannot be undone. This will permanently
-													delete your data from our database.
-												</AlertDialogDescription>
-											</AlertDialogHeader>
-											<AlertDialogFooter>
-												<AlertDialogCancel>Cancel</AlertDialogCancel>
-												<AlertDialogAction
-													onClick={async () => {
-														await deleteDoc(doc(db, "projects", project.id));
-
-														toast.info("Project deleted");
-													}}
-												>
-													Continue
-												</AlertDialogAction>
-											</AlertDialogFooter>
-										</AlertDialogContent>
-									</AlertDialog>
-								</TableCell>
+					<Table>
+						{/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+						<TableHeader>
+							<TableRow>
+								<TableHead className="w-60">Title</TableHead>
+								<TableHead className="max-w-1/3">Description</TableHead>
+								<TableHead className="max-w-24">Status</TableHead>
+								<TableHead className="text-right">Actions</TableHead>
 							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</div>
+						</TableHeader>
+						<TableBody>
+							{data?.map((project) => (
+								<TableRow className="border-2" key={nanoid()}>
+									<TableCell className="border-2 p-2">
+										{project.title}
+									</TableCell>
+									<TableCell
+										className="border-2"
+										dangerouslySetInnerHTML={{
+											__html: truncateOverview(project.description, 185),
+										}}
+									/>
+									<TableCell className="border-2 p-2">
+										{project.status}
+									</TableCell>
+									<TableCell className="flex flex-row gap-2 justify-center items-center align-middle">
+										<Link to={`/admin/projects/edit/${project.id}`}>
+											<Button variant="outline">
+												<AiOutlineEdit />
+											</Button>
+										</Link>
+
+										<AlertDialog>
+											<AlertDialogTrigger asChild>
+												<Button variant="destructive">
+													<AiOutlineDelete />
+												</Button>
+											</AlertDialogTrigger>
+											<AlertDialogContent>
+												<AlertDialogHeader>
+													<AlertDialogTitle>
+														Are you absolutely sure?
+													</AlertDialogTitle>
+													<AlertDialogDescription>
+														This action cannot be undone. This will permanently
+														delete your data from our database.
+													</AlertDialogDescription>
+												</AlertDialogHeader>
+												<AlertDialogFooter>
+													<AlertDialogCancel>Cancel</AlertDialogCancel>
+													<AlertDialogAction
+														onClick={async () => {
+															await deleteDoc(doc(db, "projects", project.id));
+
+															toast.info("Project deleted");
+														}}
+													>
+														Continue
+													</AlertDialogAction>
+												</AlertDialogFooter>
+											</AlertDialogContent>
+										</AlertDialog>
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</div>
+			)}
 		</>
 	);
 };
